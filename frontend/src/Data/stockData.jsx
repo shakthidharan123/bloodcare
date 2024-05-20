1b12c35071b63ad28401ba6bddd3da217bd0dc3b
shakthidharan123 <shakthidharan123@gmail.com>
2024-05-21 00:10:29 +0530

    added stoc component

---
+++ frontend/src/Data/stockData.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Blood from '../assets/blood.jpeg'
+
+
+export const card1data=[
+    {name:'O+ve',unit:`Available unit: 1`,image:<img src={Blood} className='bl'alt='None'></img>},
+    {name:'A+ve',unit:`Available unit: 1`,image:<img src={Blood} className='bl'alt='None'></img>},
+    {name:'B+ve',unit:`Available unit: 1`,image:<img src={Blood} className='bl'alt='None'></img>},
+    {name:'O-ve',unit:`Available unit: 1`,image:<img src={Blood} className='bl'alt='None'></img>},
+    {name:'AB+ve',unit:`Available unit: 1`,image:<img src={Blood} className='bl'alt='None'></img>},
+    {name:'B-ve',unit:`Available unit: 1`,image:<img src={Blood} className='bl'alt='None'></img>},
+    {name:'AB-ve',unit:`Available unit: 1`,image:<img src={Blood} className='bl'alt='None'></img>},
+    {name:'A-ve',unit:`Available unit: 1`,image:<img src={Blood} className='bl'alt='None'></img>}
+]
